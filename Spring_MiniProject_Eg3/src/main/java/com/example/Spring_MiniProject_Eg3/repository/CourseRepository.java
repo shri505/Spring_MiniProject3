ed2d704b98fbc0c85b4ecc61010aa7d935fc2fbd
shri505 <91963440+shri505@users.noreply.github.com>
2024-08-27 18:43:02 +0530

    Delete Spring_MiniProject_Eg3 directory

--- Spring_MiniProject_Eg3/src/main/java/com/example/Spring_MiniProject_Eg3/repository/CourseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Spring_MiniProject_Eg3.repository;
-import com.example.Spring_MiniProject_Eg3.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
